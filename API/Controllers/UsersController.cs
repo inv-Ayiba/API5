555c355ea5c7ab71b8fcc538d5274e2a69eb6cc6
inv-Ayiba <tariogounga@gmail.com>
2024-09-02 16:44:23 +0100

    works

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+//13:17
+[ApiController]
+[Route("api/[controller]")]
+public class UserController(DataContext context) : ControllerBase
+{
+
+
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        var users = context.Users.ToList();
+        return users;
+    }
+    [HttpGet("{id:int}")]
+    public ActionResult<AppUser> GetUser(int id)
+    {
+        var user = context.Users.Find(id);
+
+        if (user == null) return NotFound();
+
+        return user;
+    }
+}
+ 
+
+
+
